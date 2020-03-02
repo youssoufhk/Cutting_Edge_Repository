692bf1030b313be29fb45b1335f9c6aa3ac34051
AlixMathieu <37445479+AlixMathieu@users.noreply.github.com>
2020-03-02 14:46:58 +0100

    Update create_TFmodel.py

--- create_TFmodel.py
+++ create_TFmodel.py
@@ -9,8 +9,10 @@ Created on Sat Feb 29 16:05:19 2020
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import tensorflow as tf
 import random
+
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
 
 from sklearn.model_selection import train_test_split
 
@@ -24,7 +26,7 @@ n_time = data_close.shape[0]
 n_stocks = data_close.shape[1]
 
 
-# Data treatment ,fill the NA and calculate the daily return
+# Data treatment, fill the NA and calculate the daily reture
 for j in range(0,n_stocks):
         data_close.iloc[:,j]=data_close.iloc[:,j].fillna(data_close.iloc[:,j].mean())
 rendements = np.log(data_close.values[1:,:]/data_close.values[:-1,:])
@@ -34,11 +36,12 @@ rendements = np.float32(rendements)
 X_train, X_test = train_test_split(rendements, test_size = 0.33, random_state = 42)
 
 #Constants declaration 
-X_size = X_train.shape[1]     #X_size is the number of stock
+X_size = X_train.shape[1] #X_size is the number of stock
+ntime = X_train.shape[0]
 noise_size = X_size
-batch_size = 16               #the number of date we will used for one network training
-nb_batch = int(X_size / batch_size)
-epochs = 1000              #the number of iteration
+batch_size = 32               #the number of date we will used for one network training
+nb_batch = int(X_size/ batch_size)
+epochs = 2000             #the number of iteration
 
 ##############################################
 ##############################################
@@ -51,7 +54,7 @@ def sample_noise_Gaus(n=batch_size, c_size=noise_size):
     return np.random.normal(-1,1,(n,c_size))
 
  
-def generator(Z,nb_neurone=20,reuse=False):
+def generator(Z,nb_neurone=100,reuse=False):
     """ generator structure
     Args:
         Z: The noise that is uniform or Gaussain
@@ -65,7 +68,7 @@ def generator(Z,nb_neurone=20,reuse=False):
     return output
 
 
-def discriminator(X,nb_neurone=20,reuse=False):
+def discriminator(X,nb_neurone=100,reuse=False):
     """ generator structure
     Args:
         X: The real data or generated data 
@@ -85,15 +88,18 @@ Z = tf.placeholder(tf.float32,[batch_size,noise_size])
 
 
 gen_sample = generator(Z)
+corr = tf.placeholder(tf.float32,[batch_size-1])
 real_logits = discriminator(X)
 gen_logits = discriminator(gen_sample,reuse=True)
 
+#corr = tf.transpose(tfp.stats.correlation(gen_sample))
+#corr_loss = tf.reduce_sum(corr)- tf.reduce_sum(tf.diag_part(corr))
 
 #dis_loss = min E[-log(D(X))] + E[log(1-D(G(Z)))] := real_loss + gen_loss
 #sigmoid_cross_entropy_with_logits(x,z) = z * -log(sigmoid(x)) + (1 - z) * -log(1 - sigmoid(x))
 r_loss=tf.nn.sigmoid_cross_entropy_with_logits(logits=real_logits,labels=tf.ones_like(real_logits))
 g_loss=tf.nn.sigmoid_cross_entropy_with_logits(logits=gen_logits,labels=tf.zeros_like(gen_logits))
-disc_loss = tf.reduce_mean(r_loss + g_loss )
+disc_loss = tf.reduce_mean(r_loss + g_loss)
 
 
 #gen_loss = min E[log(1-D(G(Z)))] =  max  E[log D(G(Z)] = min - E[log(D(G(Z)))]
@@ -116,16 +122,15 @@ sess.run(tf.global_variables_initializer())
 #Training process
 with tf.device('/device:GPU:0'):
     for i in range(epochs):
-        ind_X = random.sample(range(X_size),X_size)
-            
+        ind_X = random.sample(range(ntime),ntime)
         for j in range(1,nb_batch+1):   
             index = ind_X[(j-1)*batch_size:j*batch_size]
             X_batch = X_train[index]
             Z_batch = sample_noise_Gaus(batch_size, noise_size)
             _, dloss = sess.run([disc_step, disc_loss], feed_dict={X: X_batch, Z: Z_batch})
             _, gloss = sess.run([gen_step, gen_loss], feed_dict={Z: Z_batch})
-        
-        print ("Iterations:" ,i,"Discriminator loss: ",dloss,"t Generator loss:" , gloss)
+        if i%100==0:
+            print ("Iterations:" ,i,"Discriminator loss: ",dloss,"Generator loss:" , gloss)
         
 
         
@@ -143,7 +148,7 @@ y_pred=sess.run(real_logits,feed_dict={X: pred})
 Prob_pred=sess.run(tf.sigmoid(y_pred))
 
 
-#Check the cov matrix (problem need to be solved)
-Cov_pred = np.corrcoef(pred)
+#Check the cov matrix (problem need to solve)
+Cov_pred = np.corrcoef(np.transpose(pred))
 #
-Cov_X = np.corrcoef(X_batch)
+Cov_X = np.corrcoef(np.transpose(X_batch))
